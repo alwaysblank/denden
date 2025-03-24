8c3c7a6f9efc88f79065608e40a486b698e56155
Ben Martinez-Bateman <ben@alwaysblank.dev>
2025-03-24 11:35:36 -0700

    Improve test coverage

--- tests/hub.test.ts
+++ tests/hub.test.ts
@@ -1,4 +1,5 @@
 import Hub from '../src/hub';
+import Message from '../src/message';
 
 test('Create a hub', () => {
 	const hub = new Hub();
@@ -32,9 +33,39 @@ test('Subscribe and receive old', () => {
 	const hub = new Hub();
 	const callback = jest.fn();
 	hub.pub('test', 'sandwich');
-	hub.sub('test', callback, 1);
+	const unsub1 = hub.sub('test', callback, 1);
 	expect(callback).toHaveBeenCalledTimes(1);
 	expect(callback).toHaveBeenCalledWith('sandwich');
+	unsub1(); // Remote first sub.
+	hub.pub('test', 'hamburger');
+	hub.pub('test', 'salad');
+	hub.pub('test', 'ice cream');
+	expect(hub.getMessages('test')).toHaveLength(4);
+	const callback2items = jest.fn();
+	const callbackAllItems = jest.fn();
+	const callbackNegativeItems = jest.fn();
+
+	hub.sub('test', callback2items, 2);
+	expect(callback2items).toHaveBeenCalledTimes(2);
+	expect(callback2items).toHaveBeenCalledWith('ice cream');
+	expect(callback2items).toHaveBeenCalledWith('salad');
+	expect(callback2items).not.toHaveBeenCalledWith('hamburger');
+	expect(callback2items).not.toHaveBeenCalledWith('sandwich');
+
+
+	hub.sub('test', callbackAllItems, Infinity);
+	expect(callbackAllItems).toHaveBeenCalledTimes(4);
+	expect(callbackAllItems).toHaveBeenCalledWith('ice cream');
+	expect(callbackAllItems).toHaveBeenCalledWith('salad');
+	expect(callbackAllItems).toHaveBeenCalledWith('hamburger');
+	expect(callbackAllItems).toHaveBeenCalledWith('sandwich');
+
+	hub.sub('test', callbackNegativeItems, -2);
+	expect(callbackNegativeItems).toHaveBeenCalledTimes(2);
+	expect(callbackNegativeItems).toHaveBeenCalledWith('hamburger');
+	expect(callbackNegativeItems).toHaveBeenCalledWith('sandwich');
+	expect(callbackNegativeItems).not.toHaveBeenCalledWith('ice cream');
+	expect(callbackNegativeItems).not.toHaveBeenCalledWith('salad');
 });
 
 test('Subscribe and don\'t receive old', () => {
@@ -57,8 +88,21 @@ test('Subscribe and unsubscribe', () => {
 	hub.pub('test', 'hamburger');
 	expect(callback).toHaveBeenCalledTimes(1);
 	expect(callback).toHaveBeenCalledWith('sandwich');
-	expect(hub.getMessages('test')).toHaveLength(2)
+	expect(hub.getMessages('test')).toHaveLength(2);
 });
+
+test('Subscribe and receive nothing from non-hub events', () => {
+	const hub = new Hub();
+	const callback = jest.fn();
+	hub.sub('test', callback)
+	hub.dispatchEvent(new Event('test'));
+	expect(callback).toHaveBeenCalledTimes(0);
+	hub.pub('sausage', 'sandwich');
+	expect(callback).toHaveBeenCalledTimes(0);
+	const evt = new Event(Message.NAME);
+	hub.dispatchEvent(evt);
+	expect(callback).toHaveBeenCalledTimes(0);
+})
 
 test('Watch another event dispatcher', () => {
 	const emitter = new EventTarget();
@@ -83,9 +127,11 @@ test('Get past messages', () => {
 	hub.pub('test', 'salad');
 	expect(hub.getMessages('test')).toHaveLength(3); // Default is to return all.
 	expect(hub.getMessages('test', Infinity)).toHaveLength(3);
+	expect(hub.getMessages('test', -Infinity)).toHaveLength(3);
 	expect(hub.getMessages('test', 1)).toHaveLength(1);
 	expect(hub.getMessages('test', -1)).toHaveLength(1);
 	expect(hub.getMessages('test', 2)).toHaveLength(2);
 	expect(hub.getMessages('test', 1)[0].payload).toBe('salad'); // Should return last item.
 	expect(hub.getMessages('test', -1)[0].payload).toBe('sandwich'); // Should return first item.
+	expect(hub.getMessages('does not exist', 10)).toStrictEqual([]);
 })
