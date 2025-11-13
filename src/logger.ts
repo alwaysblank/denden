c5384aa743c32757684cadb9818e471a81de3350
Ben Martinez-Bateman <ben@alwaysblank.dev>
2025-11-12 19:36:43 -0800

    WIP

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,35 @@
-export function log(level: 'ERROR'|'DEBUG'|'INFO', message: string, data?: any) {
+export const LogLevel = {
+	FATAL: 0,
+	ERROR: 1,
+	WARN: 2,
+	INFO: 3,
+	DEBUG: 4,
+} as const;
+
+/**
+ * Log a message.
+ */
+export function log(level: keyof typeof LogLevel, message: string, data?: any) {
+	dispatchEvent(new CustomEvent('denden-log', {detail: {...arguments}}));
+
+	if (dendenHideLogs) {
+		return;
+	}
+
 	switch (level) {
-		case 'ERROR':
+		case 'FATAL':
+			console.error(`💣 FATAL ERROR: ${message}`, data);
+			break;
+		case "ERROR":
 			console.error(message, data);
 			break;
-		case 'INFO':
+		case 'WARN':
+			console.warn(message, data);
+			break;
+		case "INFO":
 			console.info(message, data);
 			break;
-		case 'DEBUG':
+		case "DEBUG":
 			console.debug(message, data);
 			break;
 	}
