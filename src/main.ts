b73fc027883a077e68bc8f7e53e984cdc98eb385
Ben <ben@alwaysblank.dev>
2026-01-14 09:12:50 -0800

    Add simple preload system for browser export

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Hub from './hub';
+import Queue, {QueueRecord} from "./queue";
 
 declare global {
 	interface Window {
 		denden: Hub;
+        _dendenQueue: QueueRecord<unknown>[];
 	}
 }
 
 window.denden = new Hub();
+if ('_dendenQueue' in window && Array.isArray(window._dendenQueue)) {
+    window._dendenQueue = Queue(window._dendenQueue, window.denden);
+}
