2184829042b45e384d1b1406afa531b5de6ffb8e
Ben Martinez-Bateman <ben@alwaysblank.dev>
2026-01-29 07:44:46 -0800

    Allow complex routes for `watch`

--- src/extensions/watch.ts
+++ src/extensions/watch.ts
@@ -1,4 +1,4 @@
-import type {Hub} from '../core';
+import type {ChannelRoute, Hub} from '../core';
 
 /**
  * Callback to {@link watch} to convert an event to a payload suitable for dispatch to a channel.
@@ -11,7 +11,7 @@ export type WatchProcessor<Payload> = (event: Event) => Payload;
  * Watch an event emitter and retransmit events to a channel.
  *
  * @param hub Hub to which watched events will be dispatched.
- * @param channel The name of the channel to dispatch to. Will be created if it doesn't exit.
+ * @param channel The name of the channel(s) to dispatch to. Will create fully qualified routes if they do not already exist.
  * @param target EventTarget to watch for events of {@link eventType}.
  * @param eventType Type of event to watch for on {@link target}.
  * @param [processor] Callback to convert events from {@link target} into the appropriate payload for {@link channel}. Defaults to simply returning the entire event object.
@@ -23,11 +23,11 @@ export type WatchProcessor<Payload> = (event: Event) => Payload;
  *
  * watch(hub, 'sandwiches', window, 'sandwich');
  *
- * hub.sub('sandwiches', (p) => console.log(`received: ${p}`));
+ * hub.sub('sandwiches', (p) => console.log(`received: ${p.detail}`));
  * window.dispatchEvent(new CustomEvent('sandwich', {detail: 'reuben'}));
  * // "received: reuben"
  */
-export function watch<Payload>(hub: Hub, channel: string, target: EventTarget, eventType: string, processor?: WatchProcessor<Payload>) {
+export function watch<Payload>(hub: Hub, channel: ChannelRoute|ChannelRoute[], target: EventTarget, eventType: string, processor?: WatchProcessor<Payload>) {
 	if ('function' !== typeof processor) {
 		// Default to just passing along the event as-is.
 		processor = (event) => event as Payload;
